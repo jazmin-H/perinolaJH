d8ea57ebc59416f41489d3b2cc201bb49ff4a2d3
jazmin-H <huaynocajazmin999@gmail.com>
2024-09-16 12:03:10 -0300

    hice los test para verificarlos metodos de la clase perinola

---
+++ test/test_perinola.py
@@ -0,0 +1,19 @@
+from classPerinola import Perinola 
+
+def test_prueba():
+    assert(True)
+
+def test_constructor():
+    p = Perinola()
+    assert(p.cara_visible == p.cara_visible)
+
+def test_repr():
+    p = Perinola()
+    msj = p.__repr__()
+    assert("Salio:" in msj)
+    assert(p.cara_visible in msj)
+
+def test_tirar():
+    p = Perinola()
+    tirar = p.tirar()
+    assert(p.cara_visible == tirar)
